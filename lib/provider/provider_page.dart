167c696f20de495f14a74fb4963fc13a2e96562f
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-06-08 23:12:35 +0600

    add provider

---
+++ lib/provider/provider_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProviderPage extends StatefulWidget {
+  const ProviderPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProviderPage> createState() => _ProviderPageState();
+}
+
+class _ProviderPageState extends State<ProviderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
