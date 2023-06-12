89dc7a988b3f555a873e3011ac03a745482f200e
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-06-12 15:07:44 +0600

    add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,9 +14,69 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        color: Colors.teal,
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.red,
+            )),
+            Expanded(
+                child: Container(
+              color: Colors.teal,
+              child: Column(
+                children: [
+                  Expanded(flex: 3, child: Container()),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.black,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(children: [
+                            Expanded(
+                                flex: 1,
+                                child: Container(
+                                  height: double.infinity,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        width: 2, color: Colors.white),
+                                    borderRadius: BorderRadius.circular(25),
+                                  ),
+                                  child: Center(
+                                      child: Text(
+                                    "skip",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  )),
+                                )),
+                            SizedBox(
+                              width: 40,
+                            ),
+                            Expanded(
+                                flex: 2,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 2, color: Colors.white),
+                                      borderRadius: BorderRadius.circular(25),
+                                      color: Colors.brown),
+                                  child: Column(
+                                    children: [
+                                      Expanded(child: Container()),
+                                      Expanded(child: Container()),
+                                    ],
+                                  ),
+                                ))
+                          ]),
+                        ),
+                      )),
+                ],
+              ),
+            )),
+          ],
+        ),
       ),
-
     );
   }
 }
