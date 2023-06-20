f82442c0571563380f8912ae2f6ab7fb3011dfee
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-06-20 23:38:08 +0600

    add popular page

---
+++ lib/poem/popular_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PopularPage extends StatefulWidget {
+  const PopularPage({Key? key}) : super(key: key);
+
+  @override
+  State<PopularPage> createState() => _PopularPageState();
+}
+
+class _PopularPageState extends State<PopularPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
