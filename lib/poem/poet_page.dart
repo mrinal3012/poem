c582ca27bc7fe64e497b71bed8805fff4eae6cae
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-06-21 23:40:46 +0600

    add poetPage

---
+++ lib/poem/poet_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PoetPage extends StatefulWidget {
+  const PoetPage({Key? key}) : super(key: key);
+
+  @override
+  State<PoetPage> createState() => _PoetPageState();
+}
+
+class _PoetPageState extends State<PoetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
