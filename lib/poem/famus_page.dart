19fc3cfd66304d999447a14ab519e203ca4303b5
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-06-22 23:57:43 +0600

    add famus page

---
+++ lib/poem/famus_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FamusPage extends StatefulWidget {
+  const FamusPage({Key? key}) : super(key: key);
+
+  @override
+  State<FamusPage> createState() => _FamusPageState();
+}
+
+class _FamusPageState extends State<FamusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
